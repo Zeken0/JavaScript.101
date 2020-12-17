07fa6a95b17de4d6202c2b35cfdc5a9d655a2f5c
A_J <Ahmed.j.jibril@users.noreply.github.com>
2020-12-17 18:19:36 +0100

    u1

--- index.js
+++ index.js
@@ -37,5 +37,20 @@
 
 // console.log(url);
 
+//-
+
+// const number = 30
+// const number2 = 20
+
+// const add = number + number2;
+// const sub = number - number2;
+// const mult = number * number2;
+// const div = number / number2;
+
+// console.log(add);
+// console.log(sub);
+// console.log(mult);
+// console.log(div);
+
 //-4
 
