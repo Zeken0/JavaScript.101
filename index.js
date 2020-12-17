3537559f29c993b2ebabf727d5426e927f5030eb
A_J <Ahmed.j.jibril@users.noreply.github.com>
2020-12-17 17:54:31 +0100

    just learning

---
+++ index.js
@@ -0,0 +1,41 @@
+//-1
+
+//document.write('hello world')
+// alert('hello world')
+// console.log('hello world')
+
+//-
+
+//document.write('hello world')
+//alert('wassup fam')
+//console.log('hello world')
+
+//-2
+
+// let a1j = "Ahmed";
+// let a2j = "Jamal";
+// let a3j = "Jibril";
+// console.log(a1j,a2j,a3j);
+// console.log(a3j,a2j,a1j);
+// console.log(a2j,a3j,a1j);
+// console.log(a3j,a1j,a2j);
+// console.log(a1j,a3j,a2j);
+// console.log(a2j,a1j,a3j);
+
+//-3
+
+// const name = "Ahmed";
+// const lastname = "Jibril";
+// let fullname = name + ' ' + lastname; ;
+
+// console.log("Hello there your full name is : " + fullname);
+
+// -
+
+// const website = "google";
+// const url = "https//www." + website + ".com"
+
+// console.log(url);
+
+//-4
+
